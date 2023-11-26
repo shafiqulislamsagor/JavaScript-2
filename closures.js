86e944858905ae426f34076025737348f43f9f29
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-11-26 22:51:42 +0600

    somthings added

--- closures.js
+++ closures.js
@@ -22,3 +22,29 @@ function add () {
 const added = add();
 
 console.log(added);
+
+
+// Class Inheritance :
+
+class Car {
+    constructor(brand) {
+        this.carname = brand;
+    }
+    present() {
+        return 'I have a ' + this.carname;
+    }
+}
+
+class Model extends Car {
+    constructor(brand, mod) {
+        super(brand);
+        this.model = mod;
+    }
+    show() {
+        return this.present() + ', it is a ' + this.model;
+    }
+}
+
+let myCar = new Model("Ford", "Mustang");
+
+console.log(myCar.show());
